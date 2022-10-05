a0ef1c914b87cbeb9e0fb3678d7dceea315ceba4
Edward Im <eddieim@gmail.com>
2022-10-05 13:09:08 -0700

    FE/added hover & active to tag filters

--- client/src/components/AllHistory.jsx
+++ client/src/components/AllHistory.jsx
@@ -7,10 +7,13 @@ const AllHistory = ({ journal }) => {
 
   const [filteredEntries, setFilteredEntries] = useState()
   const [filterToggle, setFilterToggle] = useState(false)
+  const [activeTagID, setActiveTagID] = useState(null)
+
   const { tags, _id, entries } = journal
 
   const filterTags = (id) => {
     setFilterToggle(true)
+    setActiveTagID(id)
     const filteredEntries = entries.filter(entry => entry.tags.includes(id))
     setFilteredEntries(filteredEntries)
   }
@@ -23,12 +26,12 @@ const AllHistory = ({ journal }) => {
       <h1 style={{ 'textAlign': 'center' }}>All Entries</h1>
       <h3>Click tags to filter entries</h3>
       {tags.map((tag, index) => (
-        <div className="tag-item" key={index}>
+        <div 
+          className={`tag-item ${(activeTagID === tag._id)?'active':''}`} 
+          key={index}
+          onClick={() => filterTags(tag._id)}
+        >
           <span className="text">{tag.name}</span>
-          <span
-            className="close"
-            onClick={() => filterTags(tag._id)}
-          >&times;</span>
         </div>
       ))}
       {
